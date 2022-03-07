af07b728a0a505e5a25f3c9b4876e4eb38a870d9
noahbcp <noah_piper@hotmail.com>
2022-03-07 17:05:41 +1000

    Initial commit

--- 1. Your First Shiny App/1.8.5 - ggplot2-Datasets/1.8.5 - ggplot2-datasets.R
+++ 1. Your First Shiny App/1.8.5 - ggplot2-Datasets/1.8.5 - ggplot2-datasets.R
@@ -2,27 +2,22 @@ library(shiny)
 library(ggplot2)
 
 datasets <- c("economics", "faithfuld", "seals")
-
 ui <- fluidPage(
-    selectInput(inputId = "dataset", label = "Dataset", choices = datasets),
-    verbatimTextOutput(outputId = "summary"),
-    tableOutput(outputId = "plot")
+    selectInput("dataset", "Dataset", choices = datasets),
+    verbatimTextOutput("summary"),
+    plotOutput("plot") #`tableOutput corrected to plotOutput`
 )
 
 server <- function(input, output, session) {
     dataset <- reactive({
         get(input$dataset, "package:ggplot2")
     })
-    
-    output$summary <- renderPrint({
+    output$summary <- renderPrint({ #extra 'm' in 'summary'
         summary(dataset())
     })
-    
     output$plot <- renderPlot({
-        plot(dataset()) #missing () on `dataset`
-    }, res = 96)
+        plot(dataset())},  #missing parenthesis on `dataset` reactive function
+        res = 96)
 }
 
 shinyApp(ui, server)
-
-library(shiny)
