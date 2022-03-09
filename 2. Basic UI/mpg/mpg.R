e9ade273a2aa92e6935b902b25436d324dc7a906
noahbcp <noah_piper@hotmail.com>
2022-03-09 16:47:47 +1000

    Cleaned up table output.

--- 2. Basic UI/mpg/mpg.R
+++ 2. Basic UI/mpg/mpg.R
@@ -6,23 +6,33 @@ library(tidyverse)
 ## Finally, each sublist is named so selectInput can parse them as choices
 
 cars <- ggplot2::mpg %>%
-    summarise(manufacturer, model, year) %>% 
+    summarise(manufacturer, model, year, trans) %>% 
     group_by(manufacturer) %>% 
     group_split()
-choices <- list()
+car_choices <- vector(mode = "list", length = length(cars)) #Create empty list to populate with choices
+
 i <- 1
 while (i <= length(cars)) {
-    choices[i] = list(paste(cars[[i]]$model, cars[[i]]$year))
+    car_choices[i] = list(str_to_title(paste(cars[[i]]$model, cars[[i]]$year, cars[[i]]$trans)))
     i <- i + 1
 }
-names(choices) <- unique(mpg$manufacturer)
+names(car_choices) <- str_to_title(unique(mpg$manufacturer))
 
 ui <- fluidPage(
-  selectInput(inputId = 'selected_car', label = 'Select a car')
+  selectInput(inputId = 'selected_car', label = 'Select a car', choices = car_choices),
+  dataTableOutput('mpg.selected')
 )
 
 server <- function(input, output, session) {
-  
+  ## Reactive expression that matches the selected car model to corresponding position in `mpg`
+  mpg.int <- reactive({
+    as.integer(which(simplify(car_choices) == input$selected_car))
+    })
+  ## Renders a datatable without searching that includes the selected car model's data from `mpg`
+  output$mpg.selected <- renderDataTable({
+    mpg[mpg.int(), ]}, 
+    options = list(
+      searching = FALSE))
 }
 
 shinyApp(ui, server)
